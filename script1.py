bf7c6b240e05cf342181bf81ea80ee683fb1a75c
Paulo Ledo <paulofledo@gmail.com>
2015-11-03 11:28:14 -0200

    Insert script and test

---
+++ script1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2.7
+
+def checkio(data):
+    return [x for x in data if data.count(x)>1]
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th"
+
---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.7
+import pytest
+from script1 import checkio
+
+def test_checkio():
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th"
+
